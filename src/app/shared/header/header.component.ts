0d872ab7c61184648b8bc95044ba6f9d0c11b058
mdanieladla <mariadanieladarnea@gmail.com>
2025-02-08 12:19:44 +0100

    adding html header structure and some styles, adding bootstrap

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  menuItems = [
+    { name: 'Home', index: 0, link: '/home' },
+    { name: 'Destination', index: 1, link: '/destination' },
+    { name: 'Crew', index: 2, link: '/crew' },
+    { name: 'Technology', index: 3, link: '/technology' },
+    { name: 'Contact', index: 4, link: '/contact' }
+  ]
+  
 }
