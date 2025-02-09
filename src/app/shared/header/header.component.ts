7ab8ac20848fd1841bac969430a4a3b27e753b7c
mdanieladla <mariadanieladarnea@gmail.com>
2025-02-09 11:36:34 +0100

    adding menu styles for mobile, tablet and desktop

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -9,11 +9,11 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
 
   menuItems = [
-    { name: 'Home', index: 0, link: '/home' },
-    { name: 'Destination', index: 1, link: '/destination' },
-    { name: 'Crew', index: 2, link: '/crew' },
-    { name: 'Technology', index: 3, link: '/technology' },
-    { name: 'Contact', index: 4, link: '/contact' }
+    { name: 'Home', index: '00', link: '/home' },
+    { name: 'Destination', index: '01', link: '/destination' },
+    { name: 'Crew', index: '02', link: '/crew' },
+    { name: 'Technology', index: '03', link: '/technology' },
+    { name: 'Contact', index: '04', link: '/contact' }
   ]
   
 }
