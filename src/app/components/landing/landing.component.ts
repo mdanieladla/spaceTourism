6924a9e894a4496941cd2e052d7266c8cc925826
mdanieladla <mariadanieladarnea@gmail.com>
2025-02-16 19:21:54 +0100

    adding components files and finish landing

---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  imports: [],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss'
+})
+export class LandingComponent {
+  explore() {
+    console.log('Explore the universe');
+    
+  }
+}
