ffae2e2285a3159657c1cd47711040a61af5b715
mdanieladla <mariadanieladarnea@gmail.com>
2025-02-16 19:20:22 +0100

    adding routes

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,21 +1,22 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [NgClass, MatIconModule],
+  imports: [NgClass, MatIconModule, RouterOutlet, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
   menuItems = [
-    { name: 'Home', index: '00', link: '/home' },
-    { name: 'Destination', index: '01', link: '/destination' },
-    { name: 'Crew', index: '02', link: '/crew' },
-    { name: 'Technology', index: '03', link: '/technology' },
-    { name: 'Contact', index: '04', link: '/contact' }
+    { name: 'Home', index: '00', route: '/home' },
+    { name: 'Destination', index: '01', route: '/destinations' },
+    { name: 'Crew', index: '02', route: '/crew' },
+    { name: 'Technology', index: '03', route: '/technology' },
+    // { name: 'Contact', index: '04', link: '/contact' }
   ]
   
   isMenuOpen = false;
