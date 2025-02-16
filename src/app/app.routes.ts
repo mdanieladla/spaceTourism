dec452af638c317bf9477f32e3b65017ceeae4a0
mdanieladla <mariadanieladarnea@gmail.com>
2025-02-16 19:20:55 +0100

    adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { CrewComponent } from './components/crew/crew.component';
+import { DestinationsComponent } from './components/destinations/destinations.component';
+import { TechnologyComponent } from './components/technology/technology.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: LandingComponent },
+    { path: 'destinations', component: DestinationsComponent },
+    { path: 'crew', component: CrewComponent },
+    { path: 'technology', component: TechnologyComponent },
+];
