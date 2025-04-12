12ec44667e07d6f26e735d2c24e8c5ccc37ac3c5
mdanieladla <mariadanieladarnea@gmail.com>
2025-04-12 15:22:11 +0200

    adding signup reactive form

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  public signUpForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+     this.signUpForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern('^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\\D*\\d)[A-Za-z\\d!$%@#£€*?&]{8,}$')]]
+    });
+  }
+
+
+  onSubmit() {
+    console.log(this.signUpForm.value);
+    
+    // To do: use EventEmitter with form value
+  }
 }
