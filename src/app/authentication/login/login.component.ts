5f74eb988dc3c2382e6f858c3bf5bea2525bccad
mdanieladla <mariadanieladarnea@gmail.com>
2025-04-12 15:16:44 +0200

    adding login reactive form

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+public loginForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.pattern('^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\\D*\\d)[A-Za-z\\d!$%@#£€*?&]{8,}$')]]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    
+    // To do: use EventEmitter with form value
+  }
 }
